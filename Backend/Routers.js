6cfc1b644bc608a9768cdf659c58067ea41b31b9
rajashree-guha <rajashree.625.guha@gmail.com>
2024-02-14 16:16:14 +0530

    corrected error

---
+++ Backend/Routers.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const Food  = require("./data/Schema")
+const app = express()
+const router = express.Router()
+require("dotenv").config()
+
+app.use(express.json())
+
+async function connect(){
+    await mongoose.connect(process.env.mongoUrl)
+}
+
+connect()
+.then(() => {
+    console.log("Connected to Database!!!")
+}).catch((err) => {
+    console.log("Error Connecting to Database!!!")
+})
+
+router.get("/", async (req,res) => {
+    await Food.find().then((data) => {
+        returnData = data
+        res.send(data)
+    })
+})
+
+module.exports= {router}
