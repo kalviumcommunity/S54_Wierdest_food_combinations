3d3e845a44f660334210bc64247f9a668e3fc6e6
rajashree-guha <rajashree.625.guha@gmail.com>
2024-02-16 11:25:43 +0530

    updating component

--- Frontend/App/src/components/Combinations.jsx
+++ Frontend/App/src/components/Combinations.jsx
@@ -1,345 +1,41 @@
-import React from 'react'
-import {Card,CardBody,Image,Stack,Heading,Text,Flex,Collapse,Button, Grid} from '@chakra-ui/react'
-// import momo from "../assets/chocolate_momo"
-const Combinations = () => {
-  return (
-    <div id="combinations">
-       <Heading p="80px 0" color="white">Combinations</Heading>
-       <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-       <Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card>
-        <Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card>
-        <Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card>
-        <Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card>
-        <Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card>
-        <Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card>
-        <Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card><Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card><Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card><Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card><Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card><Card maxW="sm" width={"310px"} mb={30}>
-          <CardBody>
-            {/* <Image src={momo} alt="model" borderRadius="lg" width={"270px"} /> */}
-            <Stack mt="5" spacing="3">
-              <Heading
-                size="md"
-                textAlign={"center"}
-                color="red"
-              >
-                Billy Porter
-              </Heading>
-              <Flex
-                justifyContent={"space-between"}
-                color="red"
-              >
-                <Text size="md">Oscar</Text>
-                <Text fontSize="14px">25-Feb-19</Text>
-              </Flex>
-              <Text textAlign={"center"}>
-                  Known for his bold and gender-fluid fashion choices, Billy
-                  Porter made a grand entrance at the 2019 Oscars, carried in on
-                  a litter by six shirtless men. His outfit, designed by The
-                  Blonds, consisted of a custom-made tuxedo jacket and pants
-                  adorned with colorful feathers, making him a flamboyant and
-              </Text>
-            </Stack>
-          </CardBody>
-        </Card>
-       </Grid>
-    </div>
-  )
-}
+import React, { useState, useEffect } from 'react';
+import { Card, CardBody, Stack, Heading, Text, Flex, Grid } from '@chakra-ui/react';
 
-export default Combinations
+const Combinations = () => {
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    fetch('https://s54-wierdest-food-combinations.onrender.com/foodsData')
+      .then(response => response.json())
+      .then(data => {
+        setFoods(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div id="combinationsBg">
+      <Heading p="80px 0" color="white">Combinations</Heading>
+      <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+        {foods.map(food => (
+          <Card key={food.FoodId} width={"400px"} mb={30} id="combinationCard">
+            <CardBody>
+              <Stack mt="5" spacing="3">
+                <Image src={food.Image} alt={food.FoodName} style={{ borderRadius: '5px', width: '350px', height:'300px'}}></Image>
+                <Heading size="md" textAlign={"center"} color="black">
+                  {food.FoodName}
+                </Heading>  
+                  <Text size="md">{food.FoodCategory}</Text>
+                  <Text fontSize="14px">{food.Region}</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default Combinations;
